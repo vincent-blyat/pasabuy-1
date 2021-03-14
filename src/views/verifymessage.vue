8553415a9e8fffed5f17b5f322d8e5d6b88d6938
Chrizza <55417444+chrzxx@users.noreply.github.com>
2021-03-14 21:18:59 +0800

    Add files via upload

--- src/views/verifymessage.vue
+++ src/views/verifymessage.vue
@@ -4,12 +4,12 @@
     <div id="verifymsg" class="login items-center justify-center bg-gray-100 md:min-h-screen sm:max-h-screen sm:flex md:flex">
       <div class="w-4/12 my-12 overflow-hidden text-center bg-white shadow-md flex-grow-1 md:w-4/12 rounded-xl">
             <div class="px-10 py-16">
-            <img class= "sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto 2xl:mx-auto w-1/4 mb-5" src="img/id-review.svg">
+            <img class= "md:mx-auto lg:mx-auto xl:mx-auto 2xl:mx-auto w-1/4 mb-5" src="img/id-review.svg">
             <h2 class="text-2xl font-bold mb-5 md:flex items-center justify-center font-raleways ">
                 We're still reviewing your ID</h2>
-            <p class=" text-left sm:text-center md:text-right lg:text-justify xl:text-center">
+            <p class=" text-left md:text-right lg:text-justify xl:text-center">
                 This will get wrapped up as soon as possible. We'll be in touch if there's anything we need.</p>
-            <p class="mb-4 text-left sm:text-center md:text-right lg:text-justify xl:text-center">
+            <p class="mb-4 text-left md:text-right lg:text-justify xl:text-center">
                 In the meantime, you can continue setting up your profile</p>
             <div class="md:flex justify-end">
                 <button class=" h-10 px-5 m-2 text-white transition-colors duration-150 bg-red-700 rounded-3xl 
@@ -22,7 +22,8 @@
 </div>
 
 <!--Mobile Version-->
-   <div id="verifymsg" class="login items-center justify-center bg-gray-100 max-h-screen flex ">
+   <div class="mobileVersion">
+      <div id="verifymsg" class="login items-center justify-center bg-gray-100 max-h-screen flex ">
       <div class="text-center bg-white">
             <div class="px-10 py-16">
             <img class= "mx-auto w-1/4 mb-5" src="img/id-review.svg">
@@ -32,13 +33,13 @@
                 This will get wrapped up as soon as possible. We'll be in touch if there's anything we need.
                 In the meantime, you can continue setting up your profile</p>
             <div class="flex justify-end">
-                <button class=" h-10 px-5 m-2 text-white transition-colors duration-150 bg-red-700 rounded-3xl 
-                                focus:shadow-outline hover:bg-red-800 ">
+                <button class=" h-10 px-5 m-2 text-white transition-colors duration-150 bg-red-700 rounded-3xl focus:shadow-outline hover:bg-red-800 ">
                     <router-link :to="{name: 'ProfileEdit'}" >NEXT</router-link></button>
             </div>
         </div>    
     </div>
 </div>
+   </div>
 </template>
 
 
