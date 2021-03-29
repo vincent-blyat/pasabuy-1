63f8fe291244199e7d8df61e0c5bd122bc8aa3c0
lordsiinbal <79137266+lordsiinbal@users.noreply.github.com>
2021-03-29 08:27:01 +0800

    Update tailwind.config.js

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   theme: {
     extend: {
       screens: {
+         'ssm':{'min':'0px','max':'299px'},
         'vs': {'min': '300px', 'max': '639px'},
         'md': {'min': '640px', 'max': '1023px'},
         'lg': {'min': '1024px', 'max': '1279px'},
