fcaa620f0e7f28ff5b4d16dd134b1e40fb212e57
Chrizza <55417444+chrzxx@users.noreply.github.com>
2021-04-07 09:10:39 +0800

    Add files via upload

--- front-end/src/views/PostModal.vue
+++ front-end/src/views/PostModal.vue
@@ -1,13 +1,13 @@
 <template>
     <!-- Post Modal-->
-    <div v-on:click.self="close" class="bg-black font-nunito bg-opacity-25 fixed inset-0 flex ssm:px-2 vs:px-2 sm:px-2 justify-center items-center">
-     <div class="inline-flex flex-col space-y-2 bg-white shadow rounded-xl h-auto w-550 ssm:w-full vs:w-full sm:w-full">
-        <div class="flex flex-col  mt-4">
+    <div v-on:click.self="close" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-25 font-nunito ssm:px-2 vs:px-2 sm:px-2">
+     <div class="inline-flex flex-col h-auto space-y-2 bg-white shadow rounded-xl w-550 ssm:w-full vs:w-full sm:w-full">
+        <div class="flex flex-col mt-4">
 
             <!--Create Post section-->
-            <h1 class="items-center justify-center ml-24 ssm:ml-8 ssm:pl-8 vs:ml-6 pb-6 text-gray-900 leading-normal pl-20 text-2xl ssm:text-xl vs:text-xl font-bold text-center">
+            <h1 class="items-center justify-center pb-6 pl-20 ml-24 text-2xl font-bold leading-normal text-center text-gray-900 ssm:ml-8 ssm:pl-8 vs:ml-6 ssm:text-xl vs:text-xl">
                 Create Post
-                <button @click.prevent="close" class="pl-14 ml-20 ssm:pl-4 ssm:ml-4 vs:pl-4 vs:ml-8 focus:outline-none text-sm ssm:text-xs vs:text-xs mb-1 leading-none text-indigo-900">
+                <button @click.prevent="close" class="mb-1 ml-20 text-sm leading-none text-indigo-900 pl-14 ssm:pl-4 ssm:ml-4 vs:pl-4 vs:ml-8 focus:outline-none ssm:text-xs vs:text-xs">
                 Close
                 </button>
               </h1>
@@ -16,10 +16,10 @@
             <!--Shopping Offer and Order Request Button-->
              <div class="flex flex-row items-center justify-around pb-2 space-x-2 ssm:space-x-1">
                 <div @click=" isActive_function('btn1')" :class="{active: activeBtn === 0 }" class="inline-flex">
-                    <button @click="toggleShopOfferbtn" class="text-base ssm:text-sm vs:text-sm font-nunito font-bold focus:outline-none leading-none text-center text-black uppercase">Shopping Offer</button>
+                    <button @click="toggleShopOfferbtn" class="text-base font-bold leading-none text-center text-black uppercase ssm:text-sm vs:text-sm font-nunito focus:outline-none">Shopping Offer</button>
                  </div>
                  <div @click=" isActive_function('btn2')" :class="{active: activeBtn === 'btn2' }" class="inline-flex">
-                    <button @click="toggleOrderRequestbtn" class="text-base ssm:text-sm vs:text-sm font-nunito font-bold focus:outline-none leading-none text-center text-black uppercase">Order Request</button>
+                    <button @click="toggleOrderRequestbtn" class="text-base font-bold leading-none text-center text-black uppercase ssm:text-sm vs:text-sm font-nunito focus:outline-none">Order Request</button>
                  </div>
              </div>
              <hr>
