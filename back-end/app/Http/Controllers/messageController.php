1c6240a3935b2c82838efcb26c64e2ccee17e717
marsyaaaaal <marcial.abasolajr18@gmail.com>
2021-04-08 12:47:32 +0800

    edited sql

--- back-end/app/Http/Controllers/messageController.php
+++ back-end/app/Http/Controllers/messageController.php
@@ -15,7 +15,7 @@ class messageController extends Controller
     public function getChatroom()
     {
         # code...
-        return DB::select( DB::raw("SELECT firstName,lastName,email,tbl_messageroom.messageRoomNumber, tbl_messageroom.dateCreated FROM `tbl_userinformation` INNER JOIN tbl_messageroom ON tbl_messageroom.email1 = tbl_userinformation.email OR tbl_messageroom.email2 = tbl_userinformation.email WHERE tbl_messageroom.email1 = '".Auth::user()->email."' or tbl_messageroom.email2 = '".Auth::user()->email."' ORDER BY tbl_messageroom.dateCreated DESC") );
+        return DB::select( DB::raw("SELECT firstName,lastName,email,tbl_messageroom.messageRoomNumber, tbl_message.dateCreated, tbl_message.messages, tbl_message.Sender FROM `tbl_userinformation` INNER JOIN tbl_messageroom ON tbl_messageroom.email1 = tbl_userinformation.email OR tbl_messageroom.email2 = tbl_userinformation.email WHERE tbl_messageroom.email1 = '".Auth::user()->email."' or tbl_messageroom.email2 = '".Auth::user()->email."' ORDER BY tbl_messageroom.dateCreated DESC") );
         // return userInformation::select('firstName','lastName','email','dateCreated')
         //                         ->join('tbl_message', 'tbl_message.email1','=', 'tbl_userInformation.email')
         //                         ->orOn('tbl_message.email2', '=', 'tbl_userInformation.email')
