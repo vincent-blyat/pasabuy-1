6e7fcf5ba0e098c072732289b1d9a89e5e5e4c7f
lordsiinbal <79137266+lordsiinbal@users.noreply.github.com>
2021-04-08 22:38:10 +0800

    Update account_info.vue

--- front-end/src/views/account_info.vue
+++ front-end/src/views/account_info.vue
@@ -4,7 +4,7 @@
             <div class="flex gap-4">
             <p class="font-bold font-nunito text-sm">Account Info</p>
             </div>
-           <div>  <label for="sub" id="edt3" @click="edit3=!edit3,Edit('edt3'), show=!show" class="
+           <div>  <label for="sub" id="edt3" @click="toggle=!toggle" class="
                text-blue-800 w-min font-bold text-sm cursor-pointer">Edit</label></div>
         </div> 
         <div  class="text-sm w-full gap-x-10 pt-8 space-y-8
@@ -15,22 +15,49 @@
         <span class="  font-raleways font-bold grid grid-cols-2 "> 
         <p class="text-gray-500">Email</p>
        <span>
-           <p v-if="show">{{account_info.email}}</p>
-           <input type="email" v-if="edit3"
-        id="input_email" v-model="account_info.email"
-        class="ring-2 ring-gray-400 font-bold w-full">
+           <p>{{account_info.email}}</p>
        </span>
         </span>
         <span class=" font-raleways font-bold  grid grid-cols-2"> 
         <p  class="text-gray-500">Password</p>
         <span>
-            <p v-if="show" >{{account_info.password}}</p>
-            <input type="password" v-if="edit3"
-        id="input_password"  v-model="account_info.password" 
-        class="ring-2 ring-gray-400 font-bold w-full">
+            <input type="password" class="bg-transparent" :value="account_info.password" disabled/>
         </span>
         </span>
         </div>
+        <div v-if="toggle" class="fixed inset-0 h-max bg-white bg-opacity-75"></div>
+        <div v-if="toggle" class=" fixed overflow-y-auto items-center  inset-0 ">
+        <div class="flex   mt-4 w-full p-3  items-center justify-center
+        py-20
+        ">
+          <div class="  bg-white ring-1  ring-gray-300 p-5 w-full rounded-md
+             2xl:w-6/12 
+              lg:w-6/12
+              xl:w-6/12 
+              md:w-8/12
+              sm:w-10/12
+              shadow-2xl
+              h-auto
+          ">
+            <p class="text-lg font-extrabold">Edit Account Information</p>
+            <hr class="mb-5 border-2 border-red-700 bg-red-700">
+            
+          <div class=" 2xl:px-28 xl:px-28 lg:px-28 md:px-10">
+              <div class="grid grid-cols-2 gap-y-4">
+                <span>Email:</span> <input   type="text" :value="account_info.email"   class=" pl-2 bg-transparent border-2 border-gray-400"/>
+                 <span>Current Password:</span> <input type="password" placeholder="Current password"  class=" pl-2 bg-transparent border-2 border-gray-400"/>
+                 <span>New Password:</span> <input type="password" placeholder="New password"  class=" pl-2 bg-transparent border-2 border-gray-400"/>
+              
+              </div>
+            <div class="flex justify-end mt-4 space-x-4">
+              <button @click="toggle=false" class="px-3 bg-red-700 text-white rounded-2xl">Cancel</button>
+              <button @click="toggle=false" class="px-5 shadow-xl ring-1 ring-gray-300 bg-white text-red-700 rounded-2xl">Save</button>
+              
+            </div>
+          </div>
+          </div>
+        </div>
+    </div>
     </div>
 </template>
 <script>
@@ -38,6 +65,7 @@ export default {
    
 data(){
     return{
+        toggle:false,
     disabled: 0,
     hidden:false,
     show:true,
@@ -53,6 +81,7 @@ data(){
   
 },
 methods:{
+
     submit () {
        
     },
@@ -67,6 +96,7 @@ methods:{
          
       }
     }
+
 }
-} 
+}
 </script>
