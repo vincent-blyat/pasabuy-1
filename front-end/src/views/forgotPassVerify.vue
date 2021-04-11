a898b36033b63a58a6e5eb5eb3f7bc308e528e72
Chrizza <55417444+chrzxx@users.noreply.github.com>
2021-04-11 08:05:44 +0800

    Update forgotPassVerify.vue

--- front-end/src/views/forgotPassVerify.vue
+++ front-end/src/views/forgotPassVerify.vue
@@ -13,10 +13,10 @@
                 <h2 class="items-center justify-center mb-5 text-2xl font-bold md:flex font-raleways ">
                     Email has been sent!</h2>
                 <p class="text-left  md:text-right lg:text-justify xl:text-center">
-                Please check your email and click on the received link to to reset you password.</p>
+                Please check your email and click on the received link to reset you password.</p>
                 <div class="flex justify-center py-5">
                     <button class="w-full h-12 px-5 py-2 text-white transition-colors duration-150 bg-red-700 rounded-3xl focus:shadow-outline hover:bg-red-700">
-                        <router-link to="/forgot-password-verify" >Send</router-link></button>
+                        <router-link to="/forgot-password-verify" >Log in</router-link></button>
                 </div>
                 <div class="mt-4 text-left text-grey-dark">
                     Did you received the link?
