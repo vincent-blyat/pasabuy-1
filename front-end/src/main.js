fbdca5d07fe2f8bc010cc7ec73b423bcfbca2a05
llwyd-bot123 <61051378+llwyd-bot123@users.noreply.github.com>
2021-04-19 20:36:17 +0800

    Update main.js

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/css/tailwind.css'
+window.$ = window.jQuery = require('jquery');
 createApp(App).use(router).mount('#app')
