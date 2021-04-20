7272c30b13df0a377bab0ef3b3cd3449c36bb18d
lordsiinbal <79137266+lordsiinbal@users.noreply.github.com>
2021-04-20 20:29:11 +0800

    Update deliver.vue

--- front-end/src/views/deliver.vue
+++ front-end/src/views/deliver.vue
@@ -12,7 +12,7 @@ xsm:w-max
       justify-center self-center  gap-y-5 mt-16 hidden ">
       <p class="text-2xl  font-nunito font-bold uppercase">Deliveries</p>
 </div>
-<div class="mt-20  px-3 w-full flex flex-col pb-4 
+<div class="mt-20  px-3 w-full flex flex-col pb-10 
             2xl:flex-row 2xl:px-0  2xl:mt-0 2xl:self-center 2xl:justify-items-center  2xl:items-center
             lg:flex-row lg:px-0     lg:self-center lg:justify-items-center lg:mt-0
             xl:flex-row xl:px-0    xl:self-center xl:justify-items-center xl:mt-0 
@@ -25,12 +25,12 @@ xsm:w-max
               sm:w-max
              ">
     <div class="mt-3 mr-3">
-    <button @click=" isActive_function('btn1')" :class="{active: activeBtn === 0 }" type="button"  class=" text-red-700 px-3 py-1 shadow  rounded-2xl bg-white w-full h-full"><a> All Deliveries</a></button>
+    <button @click=" isActive_function('btn1')" :class="{active: activeBtn === 0 }" type="button"  class=" focus:outline-none text-red-700 px-3 py-1 shadow  rounded-2xl bg-white w-full h-full"><a> All Deliveries</a></button>
   </div>
   <div class="flex  items-center gap-x-2">
-  <button  @click=" isActive_function('btn2')" :class="{active: activeBtn === 'btn2' }"  type="button"    class=" text-green-150 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Complete</a></button>  
-  <button   @click=" isActive_function('btn3')" :class="{active: activeBtn === 'btn3' }"  type="button"   class=" text-blue-700 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Confirmed</a></button>
-  <button  @click=" isActive_function('btn4')" :class="{active: activeBtn === 'btn4' }"  type="button"    class=" text-yellow-600 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Cancelled</a></button>
+  <button  @click=" isActive_function('btn2')" :class="{active: activeBtn === 'btn2' }"  type="button"    class=" focus:outline-none text-green-150 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Complete</a></button>  
+  <button   @click=" isActive_function('btn3')" :class="{active: activeBtn === 'btn3' }"  type="button"   class=" focus:outline-none text-blue-700 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Confirmed</a></button>
+  <button  @click=" isActive_function('btn4')" :class="{active: activeBtn === 'btn4' }"  type="button"    class=" focus:outline-none text-yellow-600 px-3 py-1 shadow rounded-2xl bg-white w-full h-full"><a>Cancelled</a></button>
   </div>
 </div><div class="p-5 w-full  flex flex-col space-y-4 items-center 
 2xl:p-0
@@ -63,12 +63,8 @@ lg:p-0
             <span class="text-gray-500">Place on {{date}}</span>
             </div>
             <div class="vs:flex-col vs:space-x-2"> 
-             <select v-model="selected" id="select_option" class="">
-        <option v-for="option in options" :key="option.text" v-bind:value="option.value">
-            {{ option.text }}
-          </option>
-      </select>
-      <span class="text-blue-600 font-bold cursor-pointer" @click="update_order()">Update</span>
+             
+      <span class="text-blue-600 font-bold cursor-pointer" @click="toggle_status=!toggle_status">Update</span>
            </div>
          </div>
        <div class="flex  items-start gap-x-8 my-3  flex-col
@@ -123,6 +119,45 @@ lg:p-0
         </div>
         
       </div>
+
+   <div v-if="toggle_status" class="fixed inset-0 h-max bg-white bg-opacity-75"></div>
+        <div v-if="toggle_status"  class=" fixed overflow-y-auto   inset-0 ">
+        <div class="flex   mt-4 w-full p-3  items-center justify-center
+        py-20
+        ">
+          <div class=" bg-white ring-1  ring-gray-300  p-5 w-full rounded-xl
+             2xl:w-97 
+              lg:w-97
+              xl:w-97   xl:mr-16
+              md:w-8/12
+              sm:w-10/12
+              shadow-2xl
+              h-auto
+          ">
+            <div class="flex flex-row items-center  justify-between p-3">
+            <p class="hidden lg:block 2xl:block xl:block"></p>
+            <p class="text-lg font-extrabold xl:ml-8 lg:ml-8 2xl:ml-8">Update Delivery Status</p>
+            <p class="font-bold text-blue-700 cursor-pointer left-10" @click="toggle_status=false"> Close</p>
+          </div>
+         <hr>
+          <div class=" ">
+            <div class="flex flex-col p-3 space-y-4
+            ">
+            <span class="flex items-center space-x-2"><input type="radio" name="status" value="Completed"/><span>Completed</span></span>
+            <span class="flex items-center space-x-2"><input type="radio" name="status" value="Confirmed"/><span>Confirmed</span></span>
+            <span class="flex items-center space-x-2"><input type="radio" name="status" value="Cancelled"/><span>Cancelled</span></span>
+           
+              </div>
+            
+          </div>
+           <div class="flex justify-between mt-4 px-5 text-2xlspace-x-4 items-center">
+             <button @click="toggle_status=false" class="px-4 bg-red-buttons text-white focus:outline-none w-full h-7 shadow-xl ring-1 ring-gray-300 rounded-2xl">Update</button>
+            </div>
+          </div>
+        </div>
+    </div>
+   
+   
 </div>
  
 </div>
@@ -133,10 +168,12 @@ import Nav from '../views/Navbar.vue'
 export default {
   name: 'Deliver',
   components: {
-    Nav
+    Nav,
   },
   data(){
     return{
+      togglef:false,
+      toggle_status:false,
        activeBtn:0,
         name:'Yamete',
       rate:'4.5',
@@ -153,23 +190,6 @@ export default {
     ]
     
 }
-},
-methods:{
-update_order(){
-  if(this.selected!="Confirmed"){
-      alert(this.selected);
-  }
-  
-document.getElementById("select_option").style="hidden";
-},
-isActive_function(el){
-     if(el=='btn1'){
-      this.  activeBtn= 0;
-    }
-    else {
-        this.  activeBtn= el;
-        }
-    },
 }
 }
 </script>
